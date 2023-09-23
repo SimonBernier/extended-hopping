80574d2e82ddb4154cb3e8d48c118ccb67641b1b
SimonBernier <37701567+SimonBernier@users.noreply.github.com>
2023-09-23 11:11:54 -0400

    Update extended_hopping.jl
    Need to figure out problem with tJ site

--- infiniteMPS/extended_hopping.jl
+++ infiniteMPS/extended_hopping.jl
@@ -14,26 +14,25 @@ include(
 # VUMPS parameters
 #
 
-maxdim = 50 # Maximum bond dimension
+maxdim = 64 # Maximum bond dimension
 cutoff = 1e-6 # Singular value cutoff when increasing the bond dimension
-max_vumps_iters = 200 # Maximum number of iterations of the VUMPS algorithm at each bond dimension
-vumps_tol = 1e-5
-outer_iters = 5 # Number of times to increase the bond dimension
-localham_type = MPO # or ITensor
+max_vumps_iters = 10 # Maximum number of iterations of the VUMPS algorithm at each bond dimension
+vumps_tol = 1e-6
 conserve_qns = true
+outer_iters = 10 # Number of times to increase the bond dimension
 eager = true
 
 ##############################################################################
 # CODE BELOW HERE DOES NOT NEED TO BE MODIFIED
 #
 
-N = 3 # Number of sites in the unit cell
+N = 2 # Number of sites in the unit cell
 
 initstate(n) = isodd(n) ? "↑" : "↓"
-s = infsiteinds("Electron", N; initstate, conserve_qns)
+s = infsiteinds("Electron", N; conserve_qns, initstate)
 ψ = InfMPS(s, initstate)
 
-function ITensorInfiniteMPS.unit_cell_terms(::Model"extendedHopping"; t, g=0.0)
+function ITensorInfiniteMPS.unit_cell_terms(::Model"extendedHopping"; t=1.0, g=0.0)
     opsum = OpSum()
     # nearest neighbour hopping for a unit cell
     opsum += -t, "Cdagup", 1, "Cup", 2, "Id", 3
@@ -45,10 +44,10 @@ function ITensorInfiniteMPS.unit_cell_terms(::Model"extendedHopping"; t, g=0.0)
     opsum += -t, "Id", 1, "Cdagdn", 2, "Cdn", 3
     opsum += -t, "Id", 1, "Cdagdn", 3, "Cdn", 2
     # next-nearest neighbour hopping
-    opsum += -t, "Cdagup", 1, "Cup", 3
-    opsum += -t, "Cdagup", 3, "Cup", 1
-    opsum += -t, "Cdagdn", 1, "Cdn", 3
-    opsum += -t, "Cdagdn", 3, "Cdn", 1
+    opsum += -t, "Cdagup", 1, "Id", 2, "Cup", 3
+    opsum += -t, "Cdagup", 3, "Id", 2, "Cup", 1
+    opsum += -t, "Cdagdn", 1, "Id", 2, "Cdn", 3
+    opsum += -t, "Cdagdn", 3, "Id", 2, "Cdn", 1
     # three site g-interaction
     opsum += t*g, "Cdagup", 1, "Ntot", 2, "Cup", 3
     opsum += t*g, "Cdagup", 3, "Ntot", 2, "Cup", 1
@@ -59,14 +58,13 @@ end
 model = Model("extendedHopping")
 
 # Form the Hamiltonian
-H = InfiniteSum{MPO}(model, s; t=1.0)
+H = InfiniteSum{MPO}(model, s)
 
 # Check translational invariance
 println("\nCheck translation invariance of the initial VUMPS state")
 @show norm(contract(ψ.AL[1:N]..., ψ.C[N]) - contract(ψ.C[0], ψ.AR[1:N]...))
 
-outputlevel = 1
-vumps_kwargs = (tol=vumps_tol, maxiter=max_vumps_iters, outputlevel, eager)
+vumps_kwargs = (tol=vumps_tol, maxiter=max_vumps_iters, eager)
 subspace_expansion_kwargs = (cutoff=cutoff, maxdim=maxdim)
 
 ψ = vumps_subspace_expansion(H, ψ; outer_iters, subspace_expansion_kwargs, vumps_kwargs)
